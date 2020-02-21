811023d3b9dafb24c31cfb0fb8f177d7d259280b
elach <el.ach@gmx.de>
2020-02-21 15:15:05 +0100

    Gui weitergaemacht

--- FirmenParkplatzSystem/src/gui_Einfahrt/Controller.java
+++ FirmenParkplatzSystem/src/gui_Einfahrt/Controller.java
@@ -1,4 +1,12 @@
 package gui_Einfahrt;
 
+import javafx.scene.control.Button;
+import javafx.scene.input.MouseEvent;
+
 public class Controller {
+    public Button einparken_Button;
+
+    public void clicked_Einparken(MouseEvent mouseEvent) {
+        System.out.println("Test");
+    }
 }
--- FirmenParkplatzSystem/src/gui_Einfahrt/Test.java
+++ FirmenParkplatzSystem/src/gui_Einfahrt/Test.java
@@ -1,6 +1,7 @@
 package gui_Einfahrt;
 
 import javafx.application.Application;
+import serialConnection.ArduinoConnection;
 
 public class Test {
     public static void main(String[] args) {
