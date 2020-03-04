6299f2b34a54506be7920454935baabbc318ee0a
elach <el.ach@gmx.de>
2020-03-04 20:26:56 +0100

    Adim Console fertig

--- FirmenParkplatzSystem/src/gui_Admin/Main.java
+++ FirmenParkplatzSystem/src/gui_Admin/Main.java
@@ -1,4 +1,59 @@
 package gui_Admin;
 
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
+
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.net.Socket;
+
 public class Main {
+
+    private Socket clientSocket;
+
+    private DataInputStream dIN;
+    private DataOutputStream dOUT;
+
+
+    public Main() {
+        try {
+            clientSocket = new Socket("127.0.0.1", 9669);
+            clientSocket.setKeepAlive(true);
+
+            dIN = new DataInputStream(clientSocket.getInputStream());
+            dOUT = new DataOutputStream(clientSocket.getOutputStream());
+
+            dOUT.writeUTF("Admin");
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void starteListener(TextArea textField) {
+        new Thread(() -> {
+            try {
+                String text = new String();
+                int counter = 0;
+                while (true) {
+                    while (dIN.available() == 0){Thread.sleep(10);}
+
+                    if (counter++ == 1000) {
+                        textField.setText(text.substring(text.length() / 2, text.length()));
+                        counter = 500;
+                    }
+
+                    text = textField.getText() + "\n";
+                    textField.setText(text + "> " + dIN.readUTF());
+                    textField.setScrollTop(100000000);
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }).start();
+    }
+
 }
--- FirmenParkplatzSystem/src/server/Main.java
+++ FirmenParkplatzSystem/src/server/Main.java
@@ -5,6 +5,7 @@ import protokoll.Einfahrt;
 import java.io.*;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
 
@@ -12,6 +13,7 @@ public class Main {
 
     ServerSocket serverSocket;
 
+    List<String> textToAdmin = new ArrayList<>();
 
     public Main() {
         try {
@@ -58,24 +60,54 @@ public class Main {
                     case "Einfahrt":
                         einfahrtAbfragen(out,in);
                         break;
+                    case "Admin":
+                        admintest(out,in);
+                        break;
                 }
 
-            } catch (IOException e) {
+            } catch (IOException | InterruptedException e) {
                 e.printStackTrace();
             }
         }).start();
     }
 
-    public void einfahrtAbfragen(DataOutputStream out, DataInputStream in ) throws IOException {
+    public void admintest(DataOutputStream out, DataInputStream in ) throws IOException {
+        out.writeUTF("Connected.");
+        int index = -1;
+
+        if(textToAdmin.size()-1 >= 0){
+            for (String text: textToAdmin
+                 ) {
+                out.writeUTF(text);
+            }
+            index = textToAdmin.size()-1;
+        }
+
+        while (true){
+            int size = textToAdmin.size()-1;
+            if (size != index&&size>=0) {
+                out.writeUTF(textToAdmin.get(size));
+                index = size;
+            }else {
+                try {
+                    Thread.sleep(10);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+    }
+
+    public void einfahrtAbfragen(DataOutputStream out, DataInputStream in ) throws IOException, InterruptedException {
 
         while (true) {
             //Abfragen von server
-            while (in.available() == 0) ;
+            while (in.available() == 0){Thread.sleep(10);} ;
             String code = in.readUTF();
 
             if (code.equals( String.valueOf(Einfahrt.FAHRZEUG_ANMELDEN))) {
                 //Routine
-                System.out.println("> Neues Fahrzeug hat sich angemeldet.");
+                textToAdmin.add("Neues Fahrzeug hat sich angemeldet.");
             }
         }
     }
