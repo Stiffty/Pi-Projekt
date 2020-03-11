815aa6a56d882427498894e3ac5ed98a02eceb3b
Elias <el.ach@gmx.de>
2020-03-11 08:36:07 +0200

    Rifd Kommentiert

--- FirmenParkplatzSystem/src/server/Verwaltung/RifdKarte.java
+++ FirmenParkplatzSystem/src/server/Verwaltung/RifdKarte.java
@@ -1,4 +1,7 @@
 package server.Verwaltung;
 
 public class RifdKarte {
+    //1.Soll Einfahrt Zeit sowie eine einmalige ID Speichern können um beim Bezahl automaten den Preis berechnen zu können.
+
+    //2.Im Admin Panel sollen die Infos im zusammenhang mit den Parkplätzen einsehbar sein.
 }
